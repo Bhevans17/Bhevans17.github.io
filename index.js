ab8b4b8478dc1d36b9def3701e54d52a5e6164b9
Brenden Evans <bhevans17@gmail.com>
2020-11-22 12:42:44 -0800

    Added leaflet and UI update

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
-import { createStore } from "redux";
+(function () {
+  /* Paypal */
+  paypal
+    .Buttons({
+      createOrder: function (data, actions) {
+        return actions.order.create({
+          purchase_units: [
+            {
+              amount: {
+                value: "0.01",
+              },
+            },
+          ],
+        });
+      },
+      onApprove: function (data, actions) {
+        return actions.order.capture().then(function (details) {
+          alert("Transaction completed by " + details.payer.name.given_name);
+        });
+      },
+    })
+    .render("#paypal-button-container");
 
-let store = Redux.createStore();
+  /* Mapbox */
+  var mymap = L.map("mapid").setView(
+    [36.05603414771976, -115.12009791717792],
+    12
+  );
 
-console.log(store);
+  mymap.getContainer().addEventListener("mouseover", function () {
+    map.dragging.disable();
+  });
+
+  // Re-enable dragging when user's cursor leaves the element
+  mymap.getContainer().addEventListener("mouseout", function () {
+    map.dragging.enable();
+  });
+
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken:
+        "pk.eyJ1IjoiYnJlbmRvaWsiLCJhIjoiY2ticXpsMXlrMTBzczJ1anp4eWNzYXMycSJ9._aiJ3nb05Dxhp-Lz-ajRDg",
+    }
+  ).addTo(mymap);
+
+  var marker = L.marker([36.05603414771976, -115.12009791717792]).addTo(mymap);
+  marker.bindPopup("<b>Chef On The Road</b>").openPopup();
+})();
